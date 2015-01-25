453c2da8660eb1ebe196e96771d589eb9eb80b34
Sebastien Canart <kokotchy@gmail.com>
2015-01-25 17:52:30 +0100

    Avoid using the sieve that might be endless, better check every number if isPrime

--- prob-10-summation-of-primes.hs
+++ prob-10-summation-of-primes.hs
@@ -5,7 +5,11 @@ primes = sieve [2..]
         sieve (p:x) = p : sieve [ n | n <- x, n `mod` p > 0 ]
 
 main :: IO ()
-main = putStrLn $ show result
+main = print result
     where
         result :: Int
-        result = sum $ takeWhile (<2000000) primes
+        result = sum $ takeWhile (<2000000) $ filter isPrime [2..]
+
+
+isPrime :: Int -> Bool
+isPrime nb = not $ any ((==0) . mod nb) [2..(floor . sqrt . fromIntegral) nb]
