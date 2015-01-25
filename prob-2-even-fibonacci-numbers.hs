46a7fed340aacb38f3286bcb8c0191f9e9739abf
Sebastien Canart <sebastien.canart@onprvp.fgov.be>
2015-01-25 12:41:03 +0100

    Pass file to hlint

--- prob-2-even-fibonacci-numbers.hs
+++ prob-2-even-fibonacci-numbers.hs
@@ -3,7 +3,7 @@ fibs :: [Int]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
 main :: IO ()
-main = putStrLn $ show result
+main = print result
     where
         result :: Int
         result = sum $ takeWhile (< 4000000) $ filter even fibs
