97c138583e6a2621973fabc97f7db7d3dee8b1dd
Sebastien Canart <sebastien.canart@onprvp.fgov.be>
2015-01-25 15:35:18 +0100

    Working solution for prob 12

--- prob-12-highly-divisible-trianglar-number.hs
+++ prob-12-highly-divisible-trianglar-number.hs
@@ -1,1 +1,14 @@
-tri n = sum [1..n]
+tri :: Int -> Int
+tri n = n*(n+1) `div` 2
+
+divisors :: Int -> [Int]
+divisors nb = concatMap (\x -> [x, nb `div` x]) $ filter ((==0) . (mod nb)) [1..upperBound]
+    where
+        upperBound :: Int
+        upperBound = (floor . sqrt . fromIntegral) nb
+
+prob :: Int -> Int
+prob nbDivisors = maximum $ head $ filter ((> nbDivisors) . length) $ map (divisors . tri) [1..]
+
+main :: IO ()
+main = putStrLn $ show $ prob 500
